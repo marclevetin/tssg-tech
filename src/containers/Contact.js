f210def83d672485f4e9051eb24e1b7efcd00c74
marclevetin <marc@derbyhat.org>
2017-08-30 13:24:59 -0400

    added basic contact form.  Changed color for Navmenu for demo.  Continued to debug server without success

---
+++ src/containers/Contact.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+class Contact extends Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return(
+      <div>
+        On LinkedIn?
+
+        On Meetup.com?
+
+        Contact form?
+
+        What else needs to go here?
+      </div>
+    )
+  }
+}
+
+export default Contact;
